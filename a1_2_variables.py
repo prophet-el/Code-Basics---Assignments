8a8ed20bae52f7d0f24e25ab807bd122e84a152e
Saumay Dhaundiyal <prophet@Saumays-MacBook-Pro.local>
2023-03-01 20:50:32 +0530

    .

--- a1_2_variables.py
+++ a1_2_variables.py
@@ -1,5 +1,30 @@
-# 1. find the area of the room if the length is 500 and width is 200
-l = 500
-w = 200
-a = l * w
-print("area of the room is:",a, "square feet")
+#1. Create a variable called break and assign it a value 5. See what happens and find out the reason behind the behavior that you see.
+#break = 5
+#print(break) it is showing invalid syntax since you can not assign value to break since it is already a python keyword
+
+#2. Create two variables. One to store your birth year and one to store current year. Now calculate your age using these two variables.
+
+birth_year = 1995
+cur_year = 2022
+age = cur_year - birth_year
+print(age)
+
+#3. Store your first, middle and last name in three different variables and print your full name using these variables
+first = "Saumay"
+middle = "Subhash"
+last = "Dhaundiyal"
+print("My name is:", first + " " + middle + " " + last)
+
+#4 Answer which of these are invalid variable names: "
+_nation = 5
+print(_nation)
+#1record = 5 invalid
+record1 = 10
+print(record1)
+record_one = 15
+print(record_one)
+#record-one = 20
+#print(record-1) invalid
+#record^one = 25
+#print(record^one) invalid
+#continue = 5 invalid
